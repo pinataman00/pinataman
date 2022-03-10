15c533d466be5324d63cd2240660d75d8e7e2388
pinataman00 <96680048+pinataman00@users.noreply.github.com>
2022-03-10 22:35:56 +0900

    Add files via upload

---
+++ Java/04_제어문/src/com/control/run/Main.java
@@ -0,0 +1,15 @@
+package com.control.run;
+import com.condition.ConditionController;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		//new ConditionController().ifTest();
+		//new ConditionController().ifelseTest();
+		//new ConditionController().ifElseifTest();
+		//new ConditionController().ageMessage();
+		new ConditionController().switchTest();
+	}
+
+}
